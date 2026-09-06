2b5e6f5d4bac1ef8de54a6d58e9859ed30f1b258
VanLinLin <aas12as12as12tw@gmail.com>
2026-09-06 14:19:59 -0400

    Implement complete task

--- script.js
+++ script.js
@@ -29,14 +29,34 @@ function renderTasks() {
     for (let i = 0; i < allTasksList.length; i++) {
         // select item
         const task = allTasksList[i];
-        
-        // create a new element
-        const item = document.createElement("li");
-        
-        // set new element's text content to the task name
-        item.textContent = task.name + " - " + task.priority;
 
-        taskList.appendChild(item);
+        // create a new list element
+        const li = document.createElement("li");
+
+        // add a class to the list element if the task is completed
+        if (task.completed) {
+            li.classList.add("completed");
+        };
+
+        // create Done button
+        const doneButton = document.createElement("button");
+        doneButton.textContent = "Done";
+
+        // add event listener to the Done button
+        doneButton.addEventListener("click", () => {
+            // mark the task as completed
+            task.completed = !task.completed;
+
+            renderTasks();
+        });
+
+        // set new element's text content to the task name
+        li.textContent = task.name + " - " + task.priority;
+
+        // append the Done button to the list item
+        li.appendChild(doneButton);
+
+        taskList.appendChild(li);
 
         // show the task list in the console
         // console.log(taskList);
@@ -51,20 +71,15 @@ const taskForm = document.querySelector("#task-form");
 taskForm.addEventListener("submit", (event) => {
     // need this one to keep the page from refreshing when the form is submitted
     event.preventDefault();
-    
+
     // get the value of the input field
-    const inputValue = taskForm.querySelector("#task-input").value;
+    // trim(): remove whitespace from the beginning and end of the string
+    const inputValue = taskForm.querySelector("#task-input").value.trim();
 
-    // remove whitespace from the beginning and end of the string
-    inputValue.trim(); 
-
-    
     // get the value of the priority field
     const inputPriority = taskForm.querySelector("#priority").value;
 
-    // remove whitespace from the beginning and end of the string
-    inputPriority.trim();
-
+    // check if the input value is empty
     if (inputValue === "") {
         // use the pop up alert to tell the user that they need to enter a task name
         alert("Empty task name! Please enter a task name.");
@@ -72,14 +87,18 @@ taskForm.addEventListener("submit", (event) => {
     }
 
     // create a new task object
-    const new_task = {
+    const newTask = {
         name: inputValue,
         priority: inputPriority,
         completed: false
     };
 
     // push into the task array
-    allTasksList.push(new_task);
+    allTasksList.push(newTask);
+
+    // clear the input field
+    taskForm.querySelector("#task-input").value = "";
+
 
     renderTasks();
 });
